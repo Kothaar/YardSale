69f1018e3b8b375f6ab652908ff9c2b4a74a0173
kburton501 <kebu2@pdx.edu>
2021-03-05 18:47:19 -0800

    Node now gets apod api request

--- backend/app.js
+++ backend/app.js
@@ -1,19 +1,30 @@
 const express = require("express");
+const axios = require('axios');
+const request = require('request')
+const dotenv = require("dotenv");
+
+dotenv.config()
 
 const app = express();
 const port = 5000;
-
+const API = 'https://api.nasa.gov/planetary/apod?api_key=';
+const KEY = process.env.REACT_APP_NASA_KEY;
+const url = API+KEY
 
 app.get("/", (req, res) => res.send("Hello World!"));
 app.get("/backend", (req, res) =>{
-    res.writeHead(200, {'Content-Type': 'application/json'})
-    var jsonObj = {
-        backend: 'Backend is Connected',
-        something: 'idk what else to put'
-    }
-
-
-    res.end(JSON.stringify(jsonObj))
+    console.log("test")
+    request (
+        url,
+        function(error,response,data){
+            if(!error){
+                var json = JSON.parse(data);
+                var title = json['title']
+                console.log(title)
+                res.send(json);
+            }
+        }
+    )
 });
 
-app.listen(port, () => console.log(` test liten on port ${port}`));
+app.listen(port, () => console.log(` test listen on port ${port}`));
